9176ab08de2abb26416ea86582f840d22d305d8b
Lucas Hamilton <lucas@macobserver.com>
2021-05-12 17:44:14 -0400

    finsihed tournament, making data and graphs

--- tournament_deme.cc
+++ tournament_deme.cc
@@ -4,7 +4,7 @@ ClimbChromosome* TournamentDeme::select_parent()
 {
     //Select P parents at random, where P is a constant power of two that you choose
     int parentstochoose = parentschosen_;
-    std::uniform_real_distribution<int> dist(0, pop_.size()-1);
+    std::uniform_real_distribution<> dist(0, pop_.size()-1);
     std::vector<int> parentsvector; //A vector holding the indices of the parents to choose
     while (parentstochoose > 0){
         parentstochoose -= 1;
@@ -15,6 +15,8 @@ ClimbChromosome* TournamentDeme::select_parent()
     while (parentsvector.size() > 1){
       for (unsigned long i = 0; i < parentsvector.size(); i++){ //Iterate through every index, after one is deleted
         //Find max of first parent and parent after it
+        auto test = parentsvector[i];
+        test++;
         if (pop_[parentsvector[i]]->get_fitness() < pop_[parentsvector[i+1]]->get_fitness()){
           parentsvector.erase(parentsvector.begin() + i);
         }
